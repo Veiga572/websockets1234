2dd1129e62710b26696312745faba24048670a53
Tiago André Marques Veiga <24@CTEAEBatalha.local>
2025-05-23 18:10:31 +0100

    adiciona estilos taber.io

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@latest/dist/css/tabler.min.css">
+<script src="https://cdn.jsdelivr.net/npm/@tabler/core@latest/dist/js/tabler.min.js"></script>
